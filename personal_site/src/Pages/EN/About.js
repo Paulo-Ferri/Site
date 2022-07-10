faed2b256a063076e073657b8c1af55b5144042b
Paulo Afonso <pauloferrisouza@gmail.com>
2022-07-10 10:11:24 -0300

    update

--- personal_site/src/Pages/EN/About.js
+++ personal_site/src/Pages/EN/About.js
@@ -9,6 +9,7 @@ import cssIcon from '../../images/css-icon.png';
 
 function About() {
   return(
+    <>
       <div className="about_container" id="about_container">
         <div className="icons_about_left">
           <img className="bug_icon_about" src={bugIcon} alt="icon representing a bug" />
@@ -24,12 +25,13 @@ function About() {
         <div className="about_content">
           <p>Hello visitor, how are you? I'm Paulo Afonso, I'm a law graduate from IMED college and during my study period I participated in various work environments, both in the private and public areas. I seek to re-enter the job market through web development, a dream that I am working hard to achieve. I developed this site to share a little of my study and also to have my own space in this gigantic sea that is the web. I'm currently studying web development at Trybe by the XP class and I'm open to proposals!</p>
         </div>
-        <div className="custom-shape-divider-bottom-about">
-          <svg className="about_svg" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-          </svg>
-        </div>
       </div>
+              {/* <div className="custom-shape-divider-bottom-about">
+              <svg className="about_svg" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+              </svg>
+            </div> */}
+            </>
   );
 }
 
--- personal_site/src/Pages/PT/About.js
+++ personal_site/src/Pages/PT/About.js
@@ -24,11 +24,11 @@ function About() {
         <div className="about_content">
           <p>Olá, visitante, como vai? Sou o Paulo Afonso, sou formado em direito pela faculdade IMED e durante meu período de formação participei de vários ambientes de trabalho, tanto na área privada como na pública. Busco me reinserir no mercado de trabalho através do desenvolvimento web, um sonho que estou empenhando todos os meus esforços para alcançar. Desenvolvi esse site para compartilhar um pouco do meu estudo e também para ter meu próprio espaço nesse mar giganteso que é a web. Atualmente estudo desenvolvimento web na Trybe pela turma XP e estou aberto a propostas!</p>
         </div>
-        <div className="custom-shape-divider-bottom-about">
+        {/* <div className="custom-shape-divider-bottom-about">
           <svg className="about_svg" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
           </svg>
-        </div>
+        </div> */}
       </div>
   );
 }
